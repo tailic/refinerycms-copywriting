50bfc9fff6cb3a95e5b1833b0f1eb7fd930cfc4d
tailic <niklas@derschreiber.com>
2014-08-27 08:41:03 +0200

    hotfix server fails to start

--- lib/refinery/copywriting/engine.rb
+++ lib/refinery/copywriting/engine.rb
@@ -13,7 +13,7 @@ module Refinery
           has_many :copywriting_phrases, :dependent => :destroy, :class_name => 'Refinery::Copywriting::Phrase'
           accepts_nested_attributes_for :copywriting_phrases, :allow_destroy => false
         end
-        Decorators.register! ::Refinery::Copywriting.root
+        #Decorators.register! ::Refinery::Copywriting.root
       end
 
       before_inclusion do
